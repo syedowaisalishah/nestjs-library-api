9a0be8d2c4c08ed3634bd0152238cccc63c4af35
syedowaisalishah <alishahowais@gmail.com>
2025-07-24 18:44:00 +0500

    remove extra try catch becuz already use global error middleware

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthorDto, CreateAuthorZodSchema } from '../author/dto/create-author.dto';
-import { LoginAuthorDto, LoginAuthorZodSchema } from '../author/dto/login-author.dto';
-import { internal, badRequest } from '@hapi/boom';
-import { ZodError } from 'zod';
+import { CreateAuthorDto } from '../author/dto/create-author.dto';
+import { LoginAuthorDto } from '../author/dto/login-author.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,23 +9,11 @@ export class AuthController {
 
   @Post('signup')
   async signup(@Body() body: CreateAuthorDto) {
-    try {
-      return this.authService.signup(body);
-    } catch (err) {
-      throw internal('Unexpected error');
-    }
+    return this.authService.signup(body);
   }
 
   @Post('login')
-  async login(@Body() body: any) {
-    try {
-      const dto: LoginAuthorDto = LoginAuthorZodSchema.parse(body);
-      return this.authService.login(dto);
-    } catch (err) {
-      if (err instanceof ZodError) {
-        throw badRequest(err.errors[0].message);
-      }
-      throw internal('Unexpected error');
-    }
+  async login(@Body() body: LoginAuthorDto) {
+    return this.authService.login(body);
   }
 }
