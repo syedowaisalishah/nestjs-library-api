a01957759158ddcdef3e72cf8d178792f946844b
syedowaisalishah <alishahowais@gmail.com>
2025-07-24 11:15:10 +0500

    Resolve all the suggestion in book.service

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,6 +1,6 @@
 // src/book/book.service.ts
 import { Injectable } from '@nestjs/common';
-import * as Boom from '@hapi/boom';
+import { conflict, internal, badRequest } from '@hapi/boom';
 import { BookRepository } from './book.repository';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
@@ -11,33 +11,30 @@ export class BookService {
   constructor(private readonly bookRepo: BookRepository) {}
 
   async create(data: CreateBookDto) {
-
-  console.log("owais")
-  const author = await this.bookRepo.findAuthorById(data.authorId);
+  const { authorId } = data
+  
+  const author = await this.bookRepo.findAuthorById(authorId);
   if (!author) {
-    throw Boom.badRequest('Author does not exist');
+    throw badRequest('Author does not exist');
   }
 
   const existingTitle = await this.bookRepo.findByTitleAndAuthor(data.title, data.authorId);
   if (existingTitle) {
-    console.log("hello")
-    throw Boom.conflict('Book with this title already exists for this author');
+    throw conflict('Book with this title already exists for this author');
   }
   console.log(existingTitle)
   const existingIsbn = await this.bookRepo.findByIsbn(data.isbn);
   if (existingIsbn) {
-    console.log("hello owais")
-    throw Boom.conflict('A book with this ISBN already exists');
+    throw conflict('A book with this ISBN already exists');
   }
   console.log(existingTitle)
 
   try {
     return await this.bookRepo.create(data);
   } catch (error) {
-    throw Boom.internal('Unexpected database error');
+    throw internal('Unexpected database error');
   }
 }
-
 
   findAll() {
     return this.bookRepo.findAll();
